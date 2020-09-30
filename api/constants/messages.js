9da3530a79e0123b7cf3f34ef6f648102cf3e578
Louis 2M <Oracions2410@gmail.com>
2020-09-30 02:10:41 +0100

    Finish language CRUD

---
+++ api/constants/messages.js
@@ -0,0 +1,7 @@
+module.exports = {
+    ALL_FIELDS_IS_REQUIRED_MESSAGE: 'Requête invalide, tous les champs doivent être renseignés',
+    CANNOT_CREATE_LANG = 'Impossible de créer la langue: ',
+    CANNOT_LOAD_LANGUAGES = 'Impossible de charger les langues: ',
+    LANGUAGE_NOT_FOUND = 'Le language d\'identifiant  est introuvable'
+
+}
--- app.js
+++ app.js
@@ -6,21 +6,22 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
 const languageRoutes = require('./api/routes/language')
-const { API_ROOT_PATH, LANGUAGE_ROUTES_ROOT_PATH } = require('./api/constants/url_paths')
+const { API_ROOT_PATH } = require('./api/constants/url_paths')
 
 const port = process.env.PORT
 app.use('/ping', (req, res) => {
     res.send(`Le serveur est en cours d\'exécution sur <span style="font-weight: bold;color: coral">${req.protocol}://${req.host}:${process.env.PORT}</span>`)
 })
 
+const corsOptions = {
+    origin: 'http://localhost:4200'
+}
+
 app.use(morgan('dev'))
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-
-// const log = require('./api/helpers/debug')
-// log('path', API_ROOT_PATH + LANGUAGE_ROUTES_ROOT_PATH)
 
 app.use(API_ROOT_PATH, languageRoutes)
 
